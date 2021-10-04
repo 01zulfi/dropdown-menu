4a0a70918c27e515f0f25f06de137a1354ea2220
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-10-04 17:53:26 +0500

    Create DOMFactory createList & renderDropdownMenu

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
-const dropdownButton = document.querySelectorAll("button");
-dropdownButton.forEach((button) =>
-  button.addEventListener("click", () => {
-    document.querySelector("ul").classList.toggle("active");
-  })
+function DOMFactory(element, attributes) {
+  const newElement = document.createElement(element);
+  for (const attribute in attributes) {
+    newElement[attribute] = attributes[attribute];
+  }
+  return newElement;
+}
+
+function createList(menu, menuStyles) {
+  const listParent = DOMFactory("ul", { style: menuStyles.join(";") });
+  if (!menu) return listParent;
+  for (const item of menu) {
+    const listItem = DOMFactory("li", item);
+    listParent.append(listItem);
+  }
+  return listParent;
+}
+
+function renderDropdownMenu(targetId, button, menu, menuStyles = []) {
+  const targetElement = document.getElementById(targetId);
+  const dropdownButton = DOMFactory("button", button);
+  const dropdownList = createList(menu, menuStyles);
+  targetElement.append(dropdownButton, dropdownList);
+  dropdownButton.addEventListener("click", () =>
+    dropdownList.classList.toggle("active")
+  );
+}
+
+renderDropdownMenu(
+  "dropdown",
+  {
+    textContent: "This is the dropdown",
+    className: "wow",
+  },
+  [
+    { textContent: "hkldjflakdjflajfdkljl", href: "#" },
+    { textContent: "one", href: "#" },
+    { textContent: "one", href: "#" },
+    { textContent: "one", href: "#" },
+    { textContent: "one", href: "#" },
+    { textContent: "one", href: "#" },
+  ],
+  ["background-color: black", "color: white"]
 );
+
+renderDropdownMenu("dropdown2", { textContent: "hah" }, [
+  { textContent: "why" },
+]);
